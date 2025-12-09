71d21e48b09276631de0f6634a51e4e623077abd
Adrian <adrianc.crim@hotmail.com>
2025-12-09 14:26:44 +0100

    Añadido formulario de creación de proveedores

--- obj/Debug/net9.0/InventoryAPI-UI.AssemblyInfo.cs
+++ obj/Debug/net9.0/InventoryAPI-UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryAPI-UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6055a9c1951d7eda2acaa1279d8319b581fb1db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beeec0d88f3c634f8a0a7279f565a99c4df8083a")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryAPI-UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryAPI-UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
