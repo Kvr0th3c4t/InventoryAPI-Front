5348e5088f9625712ab11048b199482a74ccab15
Adrian <adrianc.crim@hotmail.com>
2025-12-10 16:23:06 +0100

    Subida carpeta de imagenes

--- obj/Debug/net9.0/InventoryAPI-UI.AssemblyInfo.cs
+++ obj/Debug/net9.0/InventoryAPI-UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryAPI-UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a131839e46cc169e518719bc481c4da32cc9158e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dead026a45f696fa9c6acd6938a7a9c9381a4800")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryAPI-UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryAPI-UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
